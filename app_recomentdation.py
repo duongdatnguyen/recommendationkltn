1a360167e5321487839bcd9af7ba90f4844fe34a
duongdatnguyen <55284972+duongdatnguyen@users.noreply.github.com>
2022-06-21 23:49:51 +0700

    Update CORS

--- app_recomentdation.py
+++ app_recomentdation.py
@@ -5,9 +5,10 @@ import pandas as pd
 import numpy
 import os
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
